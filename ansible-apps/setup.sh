0269fcf4c1ff1115fc39ce3eb00025a7cf9d50fb
Dias1c <dias.kappasov@gmail.com>
2022-06-12 22:17:32 +0600

    finish: ansible-apps

--- ansible-apps/setup.sh
+++ ansible-apps/setup.sh
@@ -1,8 +1,6 @@
-sudo docker compose down
-sudo docker compose up -d
-# ssh-copy-id not work, why? "sh: 1: cannot create .ssh/authorized_keys: Is a directory"
-ssh-copy-id -f -p 2222 root@127.0.0.1 
-ssh-copy-id -f -p 2223 root@127.0.0.1
-ssh-copy-id -f -p 2224 root@127.0.0.1
+docker compose down
+docker compose up -d
 ansible -i hosts.ini all -m ping -u root
-ansible-playbook -i hosts.ini playbook.yml
+ansible-playbook -i hosts.ini playbook.yaml
+
+bash checker-apps.sh
